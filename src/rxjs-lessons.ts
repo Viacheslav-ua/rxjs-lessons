4ab9892d1c13e94c9eb3904d41d729ee125bfa39
Viacheslav-ua <atph@ukr.net>
2022-09-14 08:48:29 +0300

    additional fix

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -3,18 +3,46 @@ import { debounceTime, distinctUntilChanged, fromEvent, map, Observable } from "
 
 const search$ = new Observable<Event>(observer => {
   const search = document.getElementById('search')
-  if (!search) {
+  const stop = document.getElementById('stop')
+  if (!search || !stop) {
     observer.error('Element does not exist on the page')
     return
   }
-  search?.addEventListener('input', event => {
+
+  const onSearch = (event: Event) => {
+    checkSubscription()
+    console.log(123)
     observer.next(event)
-  })
+
+  }
+  const onStop = (event: Event) => {
+    checkSubscription()
+    observer.complete()
+    clear()
+  }
+
+  search?.addEventListener('input', onSearch )
+  stop?.addEventListener('click', onStop )
+
+  const checkSubscription = () => {
+    console.log('Closed', observer.closed);
+
+    if (observer.closed) {
+      clear()
+    }
+  }
+
+  const clear = () => {
+  search.removeEventListener('input', onSearch)
+  stop.removeEventListener('click', onStop)
+}
 })
+
+
 
 // const search$: Observable<Event> = fromEvent<Event>(document, 'input')
 
-search$.pipe(
+const searchSubscription = search$.pipe(
   map(event => {
     return (event.target as HTMLInputElement).value
   }),
@@ -25,7 +53,13 @@ search$.pipe(
   .subscribe({
   next: value => console.log(value),
   // error: err => console.log(err),
-  // complete: () => console.log('Complete'),
+   complete: () => console.log('Complete'),
 })
+
+setTimeout(() => {
+  console.log('Unsubscribed')
+  searchSubscription.unsubscribe()
+
+}, 10000)
 
 
